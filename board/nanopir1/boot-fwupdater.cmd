70b1656a117a2f01d5fab9b4bb3a54cc2550f726
Calin Crisan <ccrisan@gmail.com>
2020-08-17 01:45:38 +0300

    Nano Pi R1: enable overlayfs

--- board/nanopir1/boot-fwupdater.cmd
+++ board/nanopir1/boot-fwupdater.cmd
@@ -1,6 +1,6 @@
 setenv fdt_high ffffffff
 
-setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk1p2 rootwait ro no_console_suspend panic=10 quiet loglevel=1
+setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait ro no_console_suspend panic=10 quiet loglevel=1
 
 fatload mmc 0 $kernel_addr_r zImage
 fatload mmc 0 $ramdisk_addr_r uInitrd
