e148bd02d6b9e9c1c2fb1a79f93956f72c06562b
Calin Crisan <ccrisan@gmail.com>
2020-08-16 21:29:24 +0300

    Nano Pi R1: initial work

---
+++ board/nanopir1/boot-fwupdater.cmd
@@ -0,0 +1,9 @@
+setenv fdt_high ffffffff
+
+setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk1p2 rootwait ro no_console_suspend panic=10 quiet loglevel=1
+
+fatload mmc 0 $kernel_addr_r zImage
+fatload mmc 0 $ramdisk_addr_r uInitrd
+fatload mmc 0 $fdt_addr_r sun8i-h3-nanopi-r1.dtb
+
+bootz $kernel_addr_r $ramdisk_addr_r $fdt_addr_r
