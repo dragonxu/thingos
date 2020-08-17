93784eadc224c104a3cd311a0883b7b09cde2228
Calin Crisan <ccrisan@gmail.com>
2020-08-17 18:00:26 +0300

    Nano Pi R1: use precompiled u-boot image

--- board/nanopir1/boot.cmd
+++ board/nanopir1/boot.cmd
@@ -1,8 +1,13 @@
-setenv fdt_high ffffffff
+fatload mmc 0 ${kernel_addr_r} zImage
+fatload mmc 0 ${fdt_addr_r} sun8i-h3-nanopi-r1.dtb
+fatload mmc 0 ${ramdisk_addr_r} uInitrd
+fatload mmc 0 ${scriptaddr} uEnv.txt
+env import -t ${scriptaddr} ${filesize}
 
-setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait ro no_console_suspend panic=10 quiet loglevel=1
+setenv bootargs console=ttyS0,115200 earlyprintk root=${root_dev}p2 rootwait ro no_console_suspend
 
-fatload mmc 0 $kernel_addr_r zImage
-fatload mmc 0 $fdt_addr_r sun8i-h3-nanopi-r1.dtb
-
-bootz $kernel_addr_r - $fdt_addr_r
+if test "${initramfs_enabled}" = "1"; then
+    bootz ${kernel_addr_r} ${ramdisk_addr_r} ${fdt_addr_r}
+else
+    bootz ${kernel_addr_r} - ${fdt_addr_r}
+fi
--- board/nanopir1/postscript.sh
+++ board/nanopir1/postscript.sh
@@ -3,8 +3,8 @@
 set -e
 
 ${HOST_DIR}/bin/mkimage -C none -A arm -T script -d ${BOARD_DIR}/boot.cmd ${BOOT_DIR}/boot.scr
-${HOST_DIR}/bin/mkimage -C none -A arm -T script -d ${BOARD_DIR}/boot-fwupdater.cmd ${BOOT_DIR}/boot-fwupdater.scr
 
 cp ${IMG_DIR}/zImage ${BOOT_DIR}
 cp ${IMG_DIR}/sun8i-h3-nanopi-r1.dtb ${BOOT_DIR}
-cp ${BOARD_DIR}/rootfs.cpio.uboot ${BOOT_DIR}
+cp ${BOARD_DIR}/uInitrd ${BOOT_DIR}
+cp ${BOARD_DIR}/uEnv.txt ${BOOT_DIR}
